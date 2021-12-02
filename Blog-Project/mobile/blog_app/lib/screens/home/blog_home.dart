297f753d8c1db0accb1282913bdc90d782d3b61c
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2021-12-02 12:18:21 +0100

    updated mobile
    folder structure for mobile

---
+++ Blog-Project/mobile/blog_app/lib/screens/home/blog_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BlogHome extends StatelessWidget {
+  const BlogHome({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
