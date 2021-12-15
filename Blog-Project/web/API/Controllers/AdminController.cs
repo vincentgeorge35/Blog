ba485de4f9dad47e716d940b10c74ec870d570e5
ikenna charles <46247452+charlykso@users.noreply.github.com>
2021-12-15 23:48:48 +0100

    Post, Delete and Put . Adding CORS in startup

--- Blog-Project/web/API/Controllers/AdminController.cs
+++ Blog-Project/web/API/Controllers/AdminController.cs
@@ -28,12 +28,12 @@ namespace API.Controllers
             ";
 
             DataTable table = new DataTable();
-            string sqlDataSource = _configuration.GetConnectionString("BlogAppConn");
+            string sqlDataSource = _configuration.GetConnectionString("BlogAppCon");
             SqlDataReader myReader;
-            using(SqlConnection myCon = new SqlConnection(sqlDataSource) )
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
             {
                 myCon.Open();
-                using(SqlCommand myCommand = new SqlCommand(query, myCon))
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
@@ -43,6 +43,125 @@ namespace API.Controllers
             }
 
             return new JsonResult(table);
+        }
+       
+
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            string query = @"
+                select * from dbo.Admin
+                where id = @id
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BlogAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Admin admin)
+        {
+            string query = @"
+                insert into dbo.Admin
+                values(@name, @email, @password)
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BlogAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    // myCommand.Parameters.AddWithValue("@id", admin.id);
+                    myCommand.Parameters.AddWithValue("@name", admin.name);
+                    myCommand.Parameters.AddWithValue("@email", admin.email);
+                    myCommand.Parameters.AddWithValue("@password", admin.password);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Created Successfuly");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Admin admin)
+        {
+            string query = @"
+                update dbo.Admin
+                set name = @name,
+                email = @email,
+                password = @password
+                where id = @id
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BlogAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", admin.id);
+                    myCommand.Parameters.AddWithValue("@name", admin.name);
+                    myCommand.Parameters.AddWithValue("@email", admin.email);
+                    myCommand.Parameters.AddWithValue("@password", admin.password);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfuly");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                delete from dbo.Admin
+                where id = @id
+            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BlogAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@id", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfuly");
         }
     }
 }
--- Blog-Project/web/API/Startup.cs
+++ Blog-Project/web/API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace API
 {
@@ -25,6 +26,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //enable CORS
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            //JSON serializer
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+            .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
             services.AddControllers();
         }
 
